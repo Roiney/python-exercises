159b2dc2c19184dc02d39b9fe2719684383125c1
Roiney <nei_beal@hotmail.com>
2023-01-11 20:43:10 -0300

    codando

--- intensivo/aula_3/aula.py
+++ intensivo/aula_3/aula.py
@@ -1,2 +1,16 @@
 from selenium import webdriver
+import time
 
+navegador = webdriver.Chrome()
+navegador.get('https://www.google.com/')
+
+
+#buscando campo de busca e digitando
+navegador.find_element('xpath', \
+'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("cotação dolar")
+
+#dando enter
+navegador.find_element('xpath', \
+'/html/body/div[1]/div[3]/form/div[1]/div[1]/div[1]/div/div[2]/input').send_keys("")
+
+time.sleep(15)
