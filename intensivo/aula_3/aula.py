940023303e6f51eb5711471d013941dee72f5adf
Roiney <nei_beal@hotmail.com>
2023-01-11 23:41:58 -0300

    codando

--- intensivo/aula_3/aula.py
+++ intensivo/aula_3/aula.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 import time
+import pandas as pd
 
 navegador = webdriver.Chrome()
 navegador.get('https://www.google.com/')
@@ -49,7 +50,27 @@ navegador.get('https://www.melhorcambio.com/ouro-hoje')
 cotacao_ouro = navegador.find_element('xpath', \
      '//*[@id="comercial"]').\
      get_attribute("value")
-     
+
 cotacao_ouro = cotacao_ouro.replace(',', '.')
 
 print(cotacao_ouro)
+
+
+#trabalhando com o pandas
+
+tabela = pd.read_excel('Produtos.xlsx')
+
+tabela.loc[tabela["Moeda"] == "Dólar", "Cotação"] = float(cotacao_dolar)
+tabela.loc[tabela["Moeda"] == "Euro", "Cotação"] = float(cotacao_euro)
+tabela.loc[tabela["Moeda"] == "Ouro", "Cotação"] = float(cotacao_ouro)
+
+# atualizar o preço base reais (preço base original * cotação)
+tabela["Preço de Compra"] = tabela["Preço Original"] * tabela["Cotação"]
+
+# atualizar o preço final (preço base reais * Margem)
+tabela["Preço de Venda"] = tabela["Preço de Compra"] * tabela["Margem"]
+
+tabela["Preço de Venda"] = tabela["Preço de Venda"].map("R${:.2f}".format)
+tabela.to_excel('Produtos_novo.xlsx', index=False)
+
+print(tabela)
